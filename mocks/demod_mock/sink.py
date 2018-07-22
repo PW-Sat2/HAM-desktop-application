bbd5e141658a5dc8d15d01b046a1bf007fbd7cac
begreeen <michal.gumiela@gmail.com>
2018-07-22 12:00:39 +0200

    add logger to FrameSink

--- mocks/demod_mock/sink.py
+++ mocks/demod_mock/sink.py
@@ -1,13 +1,18 @@
 import zmq
+import colorlog
+import logging
 
 class FrameSink:
     def __init__(self, address="tcp://*:7001"):
         self.address = address
+        self.logger = logging.getLogger("ZMQS")
 
         self.context = zmq.Context()
         self.socket = self.context.socket(zmq.PUB)
-        print "Address: |%s|" % address
+        self.logger.log(logging.DEBUG, "Address: |{0}|".format(address))
         self.socket.bind(address)
 
     def sink(self, data):
         self.socket.send(data)
+        self.logger.log(logging.DEBUG, "Data frame sent!")
+
