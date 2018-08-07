e6e255ffada4b1c8205a9dfb8e3865b8ff657513
begreeen <michal.gumiela@gmail.com>
2018-08-07 15:30:19 +0200

    gets from queue and emits qt signals with new data

---
+++ app/queue_to_signal.py
@@ -0,0 +1,24 @@
+from PyQt4 import QtCore
+import time
+import logging
+
+
+class GetFromQueueToSignalThread(QtCore.QThread):
+
+    item_ready = QtCore.pyqtSignal(object)
+
+    def __init__(self, stop_event, timeout, gui_queue):
+        QtCore.QThread.__init__(self)
+        self.gui_queue = gui_queue
+        self.stop_event = stop_event
+        self.timeout = timeout
+        self.logger = logging.getLogger(__name__ + "." + self.__class__.__name__)
+
+    def run(self):
+        while not self.stop_event.wait(self.timeout):
+            try:
+                widget_item = self.gui_queue.pop()
+                self.item_ready.emit(widget_item)
+            except IndexError:
+                time.sleep(1)
+        self.logger.log(logging.DEBUG, "Finished GetFromQueueToSignalThread")
