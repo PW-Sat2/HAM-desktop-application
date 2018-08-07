4b445988fc557cf10e955c6ba05795404c30487a
begreeen <michal.gumiela@gmail.com>
2018-08-07 15:43:53 +0200

    move away some unrelated functions from this file

--- app/pyinstaller_hacks.py
+++ app/pyinstaller_hacks.py
@@ -12,15 +12,3 @@ def resource_path(relative_path):
         base_path = os.path.abspath(".")
 
     return os.path.join(base_path, relative_path)
-
-
-'''TO DO: Temporary, ugly thing to unpack some data from exe because of cloud side limitations in folders zipping'''
-try:
-    sys._MEIPASS
-    if not os.path.exists("saved_frames"):
-        os.makedirs("saved_frames")
-
-    for i in range(1, 6):
-        shutil.copyfile(resource_path('saved_frames/test_{0}.frames'.format(i)), 'saved_frames/test_{0}.frames'.format(i))
-except AttributeError:
-    pass
