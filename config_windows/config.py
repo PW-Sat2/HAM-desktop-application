ab6d4a3bb93cb63cc9ef40a8eafb967bbe3687f7
begreeen <michal.gumiela@gmail.com>
2018-08-23 02:49:47 +0200

    add windows config

---
+++ config_windows/config.py
@@ -0,0 +1,17 @@
+config = dict(
+    ZMQ_TARGET='localhost',
+    ZMQ_PORT=7001,
+    CLOUD_URL='http://titan.gajoch.pl:9090',
+    PING_URL='titan.gajoch.pl',
+    CREDENTIALS_FILE='credentials.json',
+    HELP_ACCOUNT='https://pw-sat.pl/projekt/komunikacja/',
+    HELP_SOURCE='https://pw-sat.pl/blog/',
+    HELP_DEMODULATOR='https://pw-sat.pl/eksperyment/',
+    HELP_UPLOAD='https://pw-sat.pl/do-pobrania/',
+    HELP_WELCOME_STEP_1='https://pw-sat.pl/eksperyment/',
+    HELP_WELCOME_STEP_2='https://pw-sat.pl/do-pobrania/',
+    HELP_WELCOME_STEP_3='https://pw-sat.pl/projekt/komunikacja/',
+    DEFAULT_SAVE_DIR='saved_frames/',
+    IQ_SOURCE=0,
+    GRC_BINARY='grc_windows/grc_part.exe'  # for Windows build
+)
