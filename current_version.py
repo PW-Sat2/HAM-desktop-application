5b5b726b51b1015ec7e794c99fcc4845a65692ee
begreeen <michal.gumiela@gmail.com>
2018-10-25 20:16:57 +0200

    prepare for 0.91 version

--- current_version.py
+++ current_version.py
@@ -1,9 +1,11 @@
 new_version = dict(
-    APP_NEW_VERSION=0.90,
+    APP_NEW_VERSION=0.91,
     WINDOW_TITLE="New version available!",
-    WINDOW_TEXT="New version (0.90) of PW-Sat2 Ground Station application is available.",
-    WINDOW_INFORMATIVE_TEXT="You can download a new version at our <a href='https://github.com/PW-Sat2/HAM-desktop-application/releases/tag/0.9'>download page</a>.",
+    WINDOW_TEXT="New version (0.91) of PW-Sat2 Ground Station application is available.",
+    WINDOW_INFORMATIVE_TEXT="You can download a new version at our "
+                            "<a href='https://github.com/PW-Sat2/HAM-desktop-application/releases/tag/0.91'>"
+                            "download page</a>.",
     WINDOW_DETAILED_TEXT="Changelog: \n"
-                         "* initial beta release\n",
-    UPDATE_URL="https://github.com/PW-Sat2/HAM-desktop-application/releases/tag/0.9"
+                         "* update module added\n",
+    UPDATE_URL="https://github.com/PW-Sat2/HAM-desktop-application/releases/tag/0.91"
 )
