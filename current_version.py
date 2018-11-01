ceb56bb169788b5aad9f4d9b1f36675bdb5a6ca4
begreeen <michal.gumiela@gmail.com>
2018-11-01 14:08:01 +0100

    prepare version files

--- current_version.py
+++ current_version.py
@@ -1,11 +1,11 @@
 new_version = dict(
-    APP_NEW_VERSION=0.96,
+    APP_NEW_VERSION=1.00,
     WINDOW_TITLE="New version available!",
-    WINDOW_TEXT="New version (0.96) of PW-Sat2 Ground Station application is available.",
+    WINDOW_TEXT="New version (1.00) of PW-Sat2 Ground Station application is available.",
     WINDOW_INFORMATIVE_TEXT="You can download a new version at our "
-                            "<a href='https://github.com/PW-Sat2/HAM-desktop-application/releases/tag/0.96'>"
+                            "<a href='https://github.com/PW-Sat2/HAM-desktop-application/releases/tag/1.00'>"
                             "download page</a>.",
     WINDOW_DETAILED_TEXT="Changelog: \n"
-                         "* Bug fix with SSL certificate on Arch\n",
-    UPDATE_URL="https://github.com/PW-Sat2/HAM-desktop-application/releases/tag/0.96"
+                         "* First release of stable version\n",
+    UPDATE_URL="https://github.com/PW-Sat2/HAM-desktop-application/releases/tag/1.00"
 )
