c59dae5aba0bc18c69dbd5eec86669af4af43974
begreeen <michal.gumiela@gmail.com>
2019-12-20 23:19:53 +0100

    prepare for v1.3

--- current_version.py
+++ current_version.py
@@ -1,12 +1,13 @@
 new_version = dict(
-    APP_NEW_VERSION=1.20,
+    APP_NEW_VERSION=1.30,
     WINDOW_TITLE="New version available!",
-    WINDOW_TEXT="New version (1.20) of PW-Sat2 Ground Station application is available.",
+    WINDOW_TEXT="New version (1.30) of PW-Sat2 Ground Station application is available.",
     WINDOW_INFORMATIVE_TEXT="You can download a new version at our "
-                            "<a href='https://github.com/PW-Sat2/HAM-desktop-application/releases/tag/1.20'>"
+                            "<a href='https://github.com/PW-Sat2/HAM-desktop-application/releases/tag/1.30'>"
                             "download page</a>.",
     WINDOW_DETAILED_TEXT="Changelog: \n"
-                         "* IQ files are no longer saved,\n"
-                         "* faster upload to server.\n",
-    UPDATE_URL="https://github.com/PW-Sat2/HAM-desktop-application/releases/tag/1.20"
+                         "* fixed bug interrupting frames upload,\n"
+                         "* deep sleep frames tagging,\n",
+                         "* a few minor improvements (e.g. decreased logging).\n",
+    UPDATE_URL="https://github.com/PW-Sat2/HAM-desktop-application/releases/tag/1.30"
 )
